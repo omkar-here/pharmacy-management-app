e07aa2e5473bcc21075cccfc07d41c6ff9a80a0c
Glenn Mendonca <glenem17@gmail.com>
2024-03-26 19:29:34 +0530

    feat: Basic components added

---
+++ api-gateway/src/test/java/com/pharmacymanagement/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharmacymanagement.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
