fb20fa8c4ad05a9b496bb57d5045dff096555c8f
omkar-causalfunnel <omkar@dev.causalfunnel.com>
2024-04-15 00:15:39 +0530

    final message

---
+++ frontend/src/components/Order/OrderPage.jsx
@@ -0,0 +1,54 @@
+// import { Table } from "flowbite-react";
+import SearchBar from "../components/SearchBar";
+import { useCallback, useState } from "react";
+import { TableComponent } from "../components/TableComponent";
+import useAxiosAuth from "../customHook/useAxiosAuth";
+import ClientPage from "../components/ClientDetails";
+export default function OrderPage() {
+  const api = useAxiosAuth();
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [order, setOrder] = useState({
+    customerId: "",
+    employeeId: 0,
+    type: "OUTGOING",
+  });
+
+  const handleSearch = useCallback(
+    async (value) => {
+      console.log(value);
+      if (value?.length > 3) {
+        try {
+          const response = await api.get(`/medicine/search?name=${value}`);
+          console.log(response.data);
+          setSearchResults(response.data.data);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    },
+    [api]
+  );
+
+  const handleSelect = (value) => {
+    setSearch(value);
+  };
+
+  return (
+    <div className="grid grid-cols-2 ">
+      {/* <SearchBar /> */}
+      <ClientPage />
+      <div className="flex flex-col gap-6 mt-7 w-full">
+        <SearchBar
+          searchResults={searchResults}
+          setSearchResults={setSearchResults}
+          handleSearch={handleSearch}
+          handleSelect={handleSelect}
+          setSearch={setSearch}
+          search={search}
+        />
+        <TableComponent data={searchResults} />
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/components/pages/HomeLayout.jsx
@@ -0,0 +1,10 @@
+// import {Outlet} from "react-router-dom";
+export default function LayoutPage() {
+  return (
+  <div>
+      <span>Nav</span>
+      {/* <div><Outlet/></div> */}
+      <span>Footer</span>
+  </div>
+  )
+  }
