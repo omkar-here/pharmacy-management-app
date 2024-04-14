730dee02cd8be733e11ac9695f9eb62d1378e7d8
Glenn Mendonca <glenem17@gmail.com>
2024-04-14 08:39:06 +0530

    feat: Api gateway

---
+++ backend/app-service/src/main/java/com/example/pharmacymanagement/appservice/controller/AnalyticsController.java
@@ -0,0 +1,10 @@
+package com.example.pharmacymanagement.appservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/analytics")
+public class AnalyticsController {
+    
+}
